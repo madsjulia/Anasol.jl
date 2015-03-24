9527a01c2c256830076eeadff460685de4f9551d
Daniel O'Malley <omalled@lanl.gov>
2015-03-24 07:50:58 -0600

    Seems to be working; needs tests, more functions with reduced params

--- anasol.jl
+++ anasol.jl
@@ -23,7 +23,7 @@ function coreexpression(dispersionname, dispersiontimedependence, i, sourcename,
 	if sourcename == "d"
 		kernelexpr = :(pdf($(symbol(string("dist", i))), (replaceme - $(symbol(string("x0", i))) - $(symbol(string("v", i))) * tau) / $(symbol(string("sigmat", i)))) / $(symbol(string("sigmat", i))))
 	elseif sourcename == "b"
-		kernelexpr = :(cdf($(symbol(string("dist", i))), (replaceme - $(symbol(string("x0", i))) - $(symbol(string("v", i))) * tau + .5 * $(symbol(string("sigma0", i)))) / $(symbol(string("sigmat", i)))) - cdf($(symbol(string("dist", i))), (x[$(i)] - $(symbol(string("x0", i))) - $(symbol(string("v", i))) * tau - .5 * $(symbol(string("sigma0", i)))) / $(symbol(string("sigmat", i)))))
+		kernelexpr = :(cdf($(symbol(string("dist", i))), (replaceme - $(symbol(string("x0", i))) - $(symbol(string("v", i))) * tau + .5 * $(symbol(string("sigma0", i)))) / $(symbol(string("sigmat", i)))) - cdf($(symbol(string("dist", i))), (replaceme - $(symbol(string("x0", i))) - $(symbol(string("v", i))) * tau - .5 * $(symbol(string("sigma0", i)))) / $(symbol(string("sigmat", i)))))
 	else
 		error("Unknown source type: $sourcename")
 	end
@@ -41,6 +41,11 @@ function coreexpression(dispersionname, dispersiontimedependence, i, sourcename,
 	return q
 end
 
+function inclosedinterval(x, a, b)
+	return x >= a && x <= b
+end
+
+shortfunctionnames = []
 maxnumberofdimensions = 2
 for n = 1:maxnumberofdimensions
 	bigq = quote
@@ -49,6 +54,7 @@ for n = 1:maxnumberofdimensions
 				@nloops numberofdimensions i ii->1:length(dispersionnames) begin
 					@nexprs numberofdimensions ii->(coreexprs_ii = coreexpression(dispersionnames[i_ii], dispersiontimedependence[i_ii], ii, sourcenames[k_ii], boundarynames[j_ii]))
 					shortfunctionname = string((@ntuple numberofdimensions ii->dispersionnames[i_ii])..., "_", (@ntuple numberofdimensions ii->sourcenames[k_ii])..., "_", (@ntuple numberofdimensions ii->boundarynames[j_ii])...)
+					shortfunctionnames = [shortfunctionnames; shortfunctionname]
 					q = quote
 						$(symbol(string("long_", shortfunctionname)))(x::Vector,tau) = 1
 					end
@@ -58,20 +64,48 @@ for n = 1:maxnumberofdimensions
 					for i = 1:numberofdimensions
 						q.args[2].args[1].args = [q.args[2].args[1].args; symbol("x0$(i)"); symbol("sigma0$(i)"); symbol("v$(i)"); symbol("sigma$(i)"); symbol("H$(i)"); symbol("xb$(i)")]
 					end
-					eval(q)
-					fullargs = copy(q.args[2].args[1].args)
+					println("q:")
+					println(q)
+					eval(q)#make the function with all possible arguments
+					#now make a version that includes a continuously released source from t0 to t1
+					continuousreleaseargs = [q.args[2].args[1].args[2:end]; symbol("lambda"); symbol("t0"); symbol("t1")]
+					#start by making the kernel of the time integral
+					qck = quote
+						function $(symbol(string("long_", shortfunctionname, "_ckernel")))(thiswillbereplaced)#this function defines the kernel that the continuous release function integrates against
+							if inclosedinterval(t - tau, t0, t1)
+								retval = exp(-lambda * tau) * $(symbol(string("long_", shortfunctionname)))($((q.args[2].args[1].args[2:end])...))
+								return retval
+							else
+								return 0.
+							end
+						end
+					end
+					qck.args[2].args[1].args = [qck.args[2].args[1].args[1]; continuousreleaseargs[1:end]...; symbol("t")]#give it the correct set of arguments
+					println("qck:")
+					println(qck)
+					eval(qck)#evaluate the kernel function definition
+					#now make a function that integrates the kernel
+					qc = quote
+						function $(symbol(string("long_", shortfunctionname, "_c")))(thiswillbereplaced)#this function defines the kernel that the continuous release function integrates against
+							return quadgk(tau->$(symbol(string("long_", shortfunctionname, "_ckernel")))($([continuousreleaseargs[1:end]...; symbol("t")]...)), 0, t)[1]
+						end
+					end
+					continuousreleaseargs[2] = symbol("t")
+					qc.args[2].args[1].args = [qc.args[2].args[1].args[1]; continuousreleaseargs[1:end]...]#give it the correct set of arguments
+					println("qc:")
+					println(qc)
+					eval(qc)
 					#now make a version that only includes the necessary arguments
+					fullargs = copy(q.args[2].args[1].args)
 					q.args[2].args[1].args = fullargs[1:3]
 					q.args[2].args[1].args[1] = symbol(shortfunctionname)
-					print(shortfunctionname, ": ")
 					for i = 4:length(fullargs)
 						argsymbol = fullargs[i]
 						if Metatools.in(argsymbol, q.args[2].args[2])
 							q.args[2].args[1].args = [q.args[2].args[1].args; argsymbol]
 						end
 					end
-					eval(q)
-					println(q.args[2].args[1].args)
+					eval(q)#make the function with only the necessary arguments
 				end
 			end
 		end
