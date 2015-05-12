1228580b47ed8a1842cf201cc5d995be9026b5d0
Daniel O'Malley <omalled@lanl.gov>
2015-05-12 14:29:11 -0600

    bugfix, improvements

--- anasol.jl
+++ anasol.jl
@@ -2,12 +2,12 @@ module Anasol
 
 using Distributions
 using Base.Cartesian
-using Metatools
+using MPTools
 
 const standardnormal = Distributions.Normal(0, 1)
 
 dispersionnames = ["b", "f"]#b is form brownian motion, f is for fractional brownian motion
-dispersiontimedependence = [s->:(sqrt($(symbol(string("sigma0", s))) + $(symbol(string("sigma", s))) * tau)), s->:(sqrt($(symbol(string("sigma0", s))) + $(symbol(string("sigma", s))) * tau ^ (2 * $(symbol(string("H", s))))))]
+dispersiontimedependence = [s->:(sqrt($(symbol(string("sigma0", s))) ^ 2 + $(symbol(string("sigma", s))) ^ 2 * tau)), s->:(sqrt($(symbol(string("sigma0", s))) ^ 2 + $(symbol(string("sigma", s))) ^ 2 * tau ^ (2 * $(symbol(string("H", s))))))]
 distributions = ["b"=>:standardnormal, "f"=>:standardnormal]
 sourcenames = ["d", "b"]#d is for distributed (e.g., gaussian or alpha stable), b is for box
 boundarynames = ["i", "r"]#d is for infinite (no boundary), r is for reflecting
@@ -28,16 +28,16 @@ function coreexpression(dispersionname, dispersiontimedependence, i, sourcename,
 		error("Unknown source type: $sourcename")
 	end
 	if boundaryname == "i"
-		Metatools.replacesymbol!(kernelexpr, :replaceme, :(x[$(i)]))
+		MPTools.replacesymbol!(kernelexpr, :replaceme, :(x[$(i)]))
 	elseif boundaryname == "r"
 		kernelexpr2 = deepcopy(kernelexpr)
-		Metatools.replacesymbol!(kernelexpr, :replaceme, :(x[$(i)]))
-		Metatools.replacesymbol!(kernelexpr2, :replaceme, :($(symbol(string("xb", i))) - x[$(i)]))
+		MPTools.replacesymbol!(kernelexpr, :replaceme, :(x[$(i)]))
+		MPTools.replacesymbol!(kernelexpr2, :replaceme, :($(symbol(string("xb", i))) - x[$(i)]))
 		kernelexpr = Expr(:call, :+, kernelexpr, kernelexpr2)
 	else
 		error("Unknown boundary condition: $boundaryname")
 	end
-	Metatools.replacesymbol!(q, :fillintheblank, kernelexpr)
+	MPTools.replacesymbol!(q, :fillintheblank, kernelexpr)
 	return q
 end
 
@@ -46,7 +46,7 @@ function inclosedinterval(x, a, b)
 end
 
 shortfunctionnames = []
-maxnumberofdimensions = 2
+maxnumberofdimensions = 3
 for n = 1:maxnumberofdimensions
 	bigq = quote
 		@nloops numberofdimensions j ii->1:length(boundarynames) begin
@@ -58,14 +58,17 @@ for n = 1:maxnumberofdimensions
 					q = quote
 						$(symbol(string("long_", shortfunctionname)))(x::Vector,tau) = 1
 					end
-					returnexpr = parse(mapreduce(i->" * retval$i", *, "return retval1", 2:numberofdimensions))
 					q.args[2].args[2].args[2] = parse(mapreduce(i->" * retval$i", *, "return retval1", 2:numberofdimensions))
 					@nexprs numberofdimensions ii->q.args[2].args[2].args = [coreexprs_ii; q.args[2].args[2].args]
 					for i = 1:numberofdimensions
 						q.args[2].args[1].args = [q.args[2].args[1].args; symbol("x0$(i)"); symbol("sigma0$(i)"); symbol("v$(i)"); symbol("sigma$(i)"); symbol("H$(i)"); symbol("xb$(i)")]
 					end
-					println("q:")
-					println(q)
+					#=
+					if shortfunctionname == "bbb_ddd_iir"
+						println("q:")
+						println(q)
+					end
+					=#
 					eval(q)#make the function with all possible arguments
 					#now make a version that includes a continuously released source from t0 to t1
 					continuousreleaseargs = [q.args[2].args[1].args[2:end]; symbol("lambda"); symbol("t0"); symbol("t1")]
@@ -81,19 +84,36 @@ for n = 1:maxnumberofdimensions
 						end
 					end
 					qck.args[2].args[1].args = [qck.args[2].args[1].args[1]; continuousreleaseargs[1:end]...; symbol("t")]#give it the correct set of arguments
-					println("qck:")
-					println(qck)
+					#=
+					if shortfunctionname == "bbb_ddd_iir"
+						println("qck:")
+						println(qck)
+					end
+					=#
 					eval(qck)#evaluate the kernel function definition
 					#now make a function that integrates the kernel
 					qc = quote
-						function $(symbol(string("long_", shortfunctionname, "_c")))(thiswillbereplaced)#this function defines the kernel that the continuous release function integrates against
-							return quadgk(tau->$(symbol(string("long_", shortfunctionname, "_ckernel")))($([continuousreleaseargs[1:end]...; symbol("t")]...)), 0, t)[1]
+						function $(symbol(string("long_", shortfunctionname, "_c")))(thiswillbereplaced)#this function defines the continuous release function
+							#these if statements let quadgk know where the discontinuities are
+							if t - t0 < 0
+								return quadgk(tau->$(symbol(string("long_", shortfunctionname, "_ckernel")))($([continuousreleaseargs[1:end]...; symbol("t")]...)), 0, t)[1]
+							elseif t - t1 < 0 && inclosedinterval(t - t0, 0, t)
+								return quadgk(tau->$(symbol(string("long_", shortfunctionname, "_ckernel")))($([continuousreleaseargs[1:end]...; symbol("t")]...)), 0, t - t0, t)[1]
+							elseif 0 < t - t1 && t - t0 < t
+								return quadgk(tau->$(symbol(string("long_", shortfunctionname, "_ckernel")))($([continuousreleaseargs[1:end]...; symbol("t")]...)), 0, t - t1, t - t0, t)[1]
+							elseif inclosedinterval(t - t1, 0, t) && t - t0 > t
+								return quadgk(tau->$(symbol(string("long_", shortfunctionname, "_ckernel")))($([continuousreleaseargs[1:end]...; symbol("t")]...)), 0, t - t1, t)[1]
+							end
 						end
 					end
 					continuousreleaseargs[2] = symbol("t")
 					qc.args[2].args[1].args = [qc.args[2].args[1].args[1]; continuousreleaseargs[1:end]...]#give it the correct set of arguments
-					println("qc:")
-					println(qc)
+					#=
+					if shortfunctionname == "bbb_ddd_iir"
+						println("qc:")
+						println(qc)
+					end
+					=#
 					eval(qc)
 					#now make a version that only includes the necessary arguments
 					fullargs = copy(q.args[2].args[1].args)
@@ -101,16 +121,22 @@ for n = 1:maxnumberofdimensions
 					q.args[2].args[1].args[1] = symbol(shortfunctionname)
 					for i = 4:length(fullargs)
 						argsymbol = fullargs[i]
-						if Metatools.in(argsymbol, q.args[2].args[2])
+						if MPTools.in(argsymbol, q.args[2].args[2])
 							q.args[2].args[1].args = [q.args[2].args[1].args; argsymbol]
 						end
 					end
+					#=
+					if shortfunctionname == "bbb_ddd_iir"
+						println("qgoodargs:")
+						println(q)
+					end
+					=#
 					eval(q)#make the function with only the necessary arguments
 				end
 			end
 		end
 	end
-	Metatools.replacesymbol!(bigq, :numberofdimensions, n)
+	MPTools.replacesymbol!(bigq, :numberofdimensions, n)
 	eval(bigq)
 end
 
